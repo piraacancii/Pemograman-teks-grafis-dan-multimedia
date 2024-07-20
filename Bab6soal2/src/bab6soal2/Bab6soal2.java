1b34f99451c4887d648d6eacb1d2e83c434e426a
piraacancii <149033514+piraacancii@users.noreply.github.com>
2024-07-20 21:18:37 +0700

    Add files via upload

---
+++ Bab6soal2/src/bab6soal2/Bab6soal2.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab6soal2;
+
+/**
+ *
+ * @author ACER
+ */
+public class Bab6soal2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int nilai= 85;
+    System.out.println("grade A");
+    System.out.println("Keterangan:Lulus");
+    
+    System.out.println("grade B");
+    System.out.println("Keterangan:Lulus");
+    }
+    
+}
