ec7287620ceadcabbe626ad210e2ed4ba02bec83
piraacancii <149033514+piraacancii@users.noreply.github.com>
2024-08-12 09:54:55 +0700

    Add files via upload

---
+++ Bab7soal1/src/bab7soal1/Bab7soal1.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab7soal1;
+
+/**
+ *
+ * @author ACER
+ */
+public class Bab7soal1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Todo code aplication logic here
+        int day= 2;
+        switch (day) {
+            case 1:
+                System.out.println("Sabtu");
+                break;
+            case 2:
+                System.out.println("minggu");
+                break;
+            
+        }
+        
+    }
+    
+}
