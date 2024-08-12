b0789442e98d95c87e2842938eb625f25466301f
piraacancii <149033514+piraacancii@users.noreply.github.com>
2024-08-12 12:20:52 +0700

    Add files via upload

---
+++ Bab8soal1/src/bab8soal1/Bab8soal1.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab8soal1;
+
+/**
+ *
+ * @author ACER
+ */
+public class Bab8soal1 {
+
+    /**
+     * @param args the command line arguments
+    */
+    public static void main(String[] args) {
+      // TODO code application logic here
+      String Awal= "Nilai Awal ?";
+      String Akhir= "Nilai Akhir ?";
+      
+      System.out.println("Nilai Awal ?");
+      System.out.println("Nilai Akhir ?");
+      
+      int arr[]={5,10,15,20,25,30,35,40,45,50};
+      for (int num : arr) {
+          System.out.println(num);
+      }
+    }
+    
+}
